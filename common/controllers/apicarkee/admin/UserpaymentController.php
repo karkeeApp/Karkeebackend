9bca200dfb115ebf1af7d7ae1cfb3e200ed944c5
Abdul <abuabdu99@gmail.com>
2022-01-10 19:20:27 +0800

    payment form updated

--- common/controllers/apicarkee/admin/UserpaymentController.php
+++ common/controllers/apicarkee/admin/UserpaymentController.php
@@ -94,11 +94,19 @@ class UserpaymentController extends Controller
         $form = $this->postLoad($form);
 
         $form->account_id = $user->account_id;
-        if (!empty($_FILES['screenshot']) AND count($_FILES['screenshot']) > 0) $form->file = UploadedFile::getInstanceByName('screenshot');
-        if (!is_null($form->file)) $form->filename = hash('crc32', $form->file->name) . time() . '.' . $form->file->extension;
-       
-        if (!empty($_FILES['log_card']) AND count($_FILES['log_card']) > 0) $form->file_logcard = UploadedFile::getInstanceByName('log_card');
+
+
+        if (!empty($_FILES['screenshot'])) $form->file = UploadedFile::getInstanceByName('screenshot');
+        if (!empty($form->file)) $form->filename = hash('crc32', $form->file->name) . time() . '.' . $form->file->extension;
+
+        if (!empty($_FILES['log_card'])) $form->file_logcard = UploadedFile::getInstanceByName('log_card');
         if (!is_null($form->file_logcard)) $form->log_card = hash('crc32', $form->file_logcard->name) . time() . '.' . $form->file_logcard->extension;
+
+        // if (!empty($_FILES['screenshot']) AND count($_FILES['screenshot']) > 0) $form->file = UploadedFile::getInstanceByName('screenshot');
+        // if (!is_null($form->file)) $form->filename = hash('crc32', $form->file->name) . time() . '.' . $form->file->extension;
+       
+        // if (!empty($_FILES['log_card']) AND count($_FILES['log_card']) > 0) $form->file_logcard = UploadedFile::getInstanceByName('log_card');
+        // if (!is_null($form->file_logcard)) $form->log_card = hash('crc32', $form->file_logcard->name) . time() . '.' . $form->file_logcard->extension;
 
         if (!$form->validate()){
             $error = self::getFirstError(ActiveForm::validate($form));
