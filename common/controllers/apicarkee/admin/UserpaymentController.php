a43b33e33d0b6902f2d78b211e6bfaf93350a4f7
Abdul <abuabdu99@gmail.com>
2022-01-11 09:53:26 +0800

    payment form updated

--- common/controllers/apicarkee/admin/UserpaymentController.php
+++ common/controllers/apicarkee/admin/UserpaymentController.php
@@ -94,7 +94,7 @@ class UserpaymentController extends Controller
         $form = $this->postLoad($form);
 
         $form->account_id = $user->account_id;
-        if (!empty($_FILES)) $form->file = UploadedFile::getInstanceByName('file');
+        if (!empty($_FILES['screenshot']) AND count($_FILES['screenshot']) > 0) $form->file = UploadedFile::getInstanceByName('screenshot');
         if (!is_null($form->file)) $form->filename = hash('crc32', $form->file->name) . time() . '.' . $form->file->extension;
        
         if (!empty($_FILES['log_card'])) $form->file_logcard = UploadedFile::getInstanceByName('log_card');
@@ -111,11 +111,23 @@ class UserpaymentController extends Controller
             $form->payment_for = !is_null($form->payment_for) ? $form->payment_for : UserPayment::PAYMENT_FOR_OTHERS;
             $userPayment = UserPayment::create($form, $user->user_id);
             
-            if (!empty($form->file)) $saved_img = Helper::saveImage($this, $form->file, $form->filename, Yii::$app->params['dir_payment']);
-            if (!empty($saved_img) AND !$saved_img['success'])  return $saved_img;
+            // if (!empty($form->file)) $saved_img = Helper::saveImage($this, $form->file, $form->filename, Yii::$app->params['dir_payment']);
+            // if (!empty($saved_img) AND !$saved_img['success'])  return $saved_img;
             
-            if (!empty($form->file_logcard)) $saved_imglc = Helper::saveImage($this, $form->file_logcard, $form->log_card, Yii::$app->params['dir_payment']);
-            if (!empty($saved_imglc) AND !$saved_imglc['success'])  return $saved_img;
+            // if (!empty($form->file_logcard)) $saved_imglc = Helper::saveImage($this, $form->file_logcard, $form->log_card, Yii::$app->params['dir_payment']);
+            // if (!empty($saved_imglc) AND !$saved_imglc['success'])  return $saved_img;
+
+            if (!empty($form->filename)){
+                $userPayment->filename = $form->filename;
+                $saved_img = Helper::saveImage($this, $form->file, $form->filename, Yii::$app->params['dir_payment']);
+            }
+            if (!empty($saved_img) AND !$saved_img['success']) return $saved_img;
+
+            if (!empty($form->file_logcard)){
+                $userPayment->log_card = $form->log_card;
+                Helper::saveImage($this, $form->file_logcard, $form->log_card, Yii::$app->params['dir_payment']);
+            }     
+
 
             if($userPayment->payment_for == UserPayment::PAYMENT_FOR_RENEWAL){
                 $dir_pay = Yii::$app->params['dir_payment'];
