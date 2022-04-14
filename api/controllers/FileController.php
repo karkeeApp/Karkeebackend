cefe91faa881237614a8e3dd65d1695208fd10ee
Frank caruana <franklincaruana@gmail.com>
2022-04-14 10:29:08 +0800

    bugfix: logging acnt

--- api/controllers/FileController.php
+++ api/controllers/FileController.php
@@ -9,6 +9,23 @@ class FileController extends \common\controllers\FileController
     public function behaviors()
     {
         return [
+            'corsFilter' => [
+                'class' => \yii\filters\Cors::class,
+                // 'cors' => [
+                //     // restrict access to
+                //     'Origin' => ['http://www.myserver.com', 'https://www.myserver.com'],
+                //     // Allow only POST and PUT methods
+                //     'Access-Control-Request-Method' => ['POST', 'PUT'],
+                //     // Allow only headers 'X-Wsse'
+                //     'Access-Control-Request-Headers' => ['X-Wsse'],
+                //     // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
+                //     'Access-Control-Allow-Credentials' => true,
+                //     // Allow OPTIONS caching
+                //     'Access-Control-Max-Age' => 3600,
+                //     // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                //     'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+                // ],
+            ],
             // 'access' => [
             //     'class' => AccessControl::class,
             //     'rules' => [
